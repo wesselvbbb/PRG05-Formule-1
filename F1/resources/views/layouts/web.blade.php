6d2ffc351a10ebfd2d81c955ecf93df257481c58
wesselvbbb <1027694@hr.nl>
2022-09-22 15:45:08 +0200

    changed partials to layout and created first database

--- F1/resources/views/layouts/web.blade.php
+++ F1/resources/views/layouts/web.blade.php
@@ -5,16 +5,17 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
     <title>F1 - @yield('title')</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
-@include('partials._nav')
+@include('layouts.nav')
 @include('partials._header')
 @yield('content')
 </body>
 <footer>
-    @include('partials._footer')
+    @include('layouts.footer')
 </footer>
 </html>
