e7352dc403ddee4cba5070f46fc44888374fee29
Wessel <1023694@hr.nl>
2022-10-05 07:51:54 +0200

    started with search function

--- F1/app/Http/Controllers/HomeController.php
+++ F1/app/Http/Controllers/HomeController.php
@@ -26,5 +26,15 @@ class HomeController extends Controller
     {
         $posts = DB::select('select * from posts');
         return view('home', ['posts' => $posts]);
+
+//        if (request('search')){
+//            $posts->where('title', 'like', '%' . request('search' . '%'));
+//        }
+//
+//        return view('posts', [
+//            'posts' => $posts->get()
+//        ]);
+
+
     }
 }
--- F1/routes/web.php
+++ F1/routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AboutController;
 use App\Http\Controllers\HelloController;
 use App\Http\Controllers\PostController;
+use App\Models\Post;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -21,7 +22,5 @@ Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('ho
 Route::get('about', [AboutController::class, 'show'])->name('about')->middleware('admin');
 
 Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::resource('/post', PostController::class);
