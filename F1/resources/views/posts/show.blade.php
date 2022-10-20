1048334eb126acb2c726f09c1e7001cdbf80a451
wesselvbbb <1027694@hr.nl>
2022-10-20 14:55:55 +0200

    view changes

--- F1/resources/views/posts/show.blade.php
+++ F1/resources/views/posts/show.blade.php
@@ -3,19 +3,17 @@
 @section('content')
     <div class="card">
         <div class="card-header">Posts Page</div>
-        <div class="card-body">
-
-
             <div class="card-body">
-                <h5 class="card-title">Title : {{ $post->title }}</h5>
-                <p class="card-text">Content : {{ $post->content }}</p>
-                <p class="card-text">Category : {{ $post->category->name }}</p>
-                <p class="card-text">Created by : {{ $post->user->name }}</p>
+                <h1 class="card-title">{{ $post->title }}</h1>
+                <img class="card-img-top" src="{{asset("storage/$post->file_path")}}" alt="Card image cap" style="max-width: 50vw; max-height: 50vh;"><br>
+                <br>
+                <p class="card-text">{{ $post->content }}</p>
+                <p class="card-text" style="color: brown">Category : {{ $post->category->name }}</p>
+                <p class="card-text" style="font-style: italic">Created by : {{ $post->user->name }}</p>
             </div>
 
             </hr>
             <a href="{{url('/post')}}">Back</a>
 
         </div>
-    </div>
-    @endsection
+@endsection
